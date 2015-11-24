e9494dd809d7bbbab107a170d1014831f0ceacf8
HereAround <M.Bies@thphys.uni-heidelberg.de>
2015-11-24 12:09:50 +0100

    Clean up of old methods

--- GradedModulePresentationsForCAP/gap/Tools.gd
+++ GradedModulePresentationsForCAP/gap/Tools.gd
@@ -11,31 +11,6 @@
 
 ####################################################################################
 ##
-#! @Section Submodule presentations
-##
-####################################################################################
-
-#! @Description
-#! The argument are a list <A>L</A> and a graded ring <A>S</A>. We then turn <A>L</A> into a matrix $M$ with entries from
-#! <A>S</A> and interpret this matrix as a mapping of a f.p. graded left $S$-module into the projective 
-#! graded <A>S</A>-module $S^n$ (with $n$ the number of columns of the matrix $M$). We return the source of this mapping.
-#! @Returns a presentation category object
-#! @Arguments M
-DeclareOperation( "SubmoduleAsGradedLeftModulePresentation",
-                  [ IsList, IsHomalgGradedRing ] );
-
-#! @Description
-#! The argument are a list <A>L</A> and a graded ring <A>S</A>. We then turn <A>L</A> into a matrix $M$ with entries from
-#! <A>S</A> and interpret this matrix as a mapping of a f.p. graded left $S$-module into the projective 
-#! graded <A>S</A>-module $S^n$ (with $n$ the number of rows of the matrix $M$). We return the source of this mapping.
-#! @Returns a presentation category object
-#! @Arguments M
-DeclareOperation( "SubmoduleAsGradedRightModulePresentation",
-                  [ IsList, IsHomalgGradedRing ] );
-
-
-####################################################################################
-##
 #! @Section Frobenius powers of presentations
 ##
 ####################################################################################
